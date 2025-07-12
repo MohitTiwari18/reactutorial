422012b31de3803285a38fd5a4dddbde5c1e6cb2
Mohit Tiwari <mohittiwari9933@gmail.com>
2025-07-12 06:13:56 +0530

    Form Handling in React

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ParentComponent from "./components/ParentComponent";
 import Greeting from "./components/Greeting";
+import Form from "./components/Form";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ParentComponent />
         {/*
         <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
@@ -31,8 +31,11 @@ class App extends Component {
         <Counter />
         <Welcome name="Mohit" heroName="Software Engineer " /> 
         <FunctionClick /> 
-        */}
+        <ParentComponent />
         <Greeting />
+        */}
+
+        <Form />
       </div>
     );
   }
